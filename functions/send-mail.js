1f60425b1126882e3b81951cf50725a233e441ae
joker <ns.ayness@gmail.com>
2023-07-27 00:31:26 +0200

    second

--- functions/send-mail.js
+++ functions/send-mail.js
@@ -1,30 +1,45 @@
 const nodemailer = require("nodemailer");
 
 exports.handler = async (event) => {
-  const message = event.body;
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "ns.ayness@gmail.com",
-      pass: "zfxtkpxqikgofgxz",
-    },
-  });
+  try {
+    console.log("try send mail");
+    console.log(event.body);
 
-  const mailOptions = {
-    from: "ns.ayness@gmail.com",
-    to: "niconico83@icloud.com", // Replace with the address of the recipient
-    subject: "New Scan Eat Client",
-    text: message,
-  };
-  const mailOptions1 = {
-    from: "ns.ayness@gmail.com",
-    to: "ns.ayness@gmail.com", // Replace with the address of the recipient
-    subject: "New Scan Eat Client",
-    text: message,
-  };
-  await transporter.sendMail(mailOptions);
-  await transporter.sendMail(mailOptions1);
+    const message = event.body;
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL_USERNAME,
+      to: "niconico83@icloud.com", // Replace with the address of the recipient
+      subject: "New Scan Eat Client",
+      text: message,
+    };
+    const mailOptions1 = {
+      from: process.env.EMAIL_USERNAME,
+      to: process.env.EMAIL_USERNAME, // Replace with the address of the recipient
+      subject: "New Scan Eat Client",
+      text: message,
+    };
+    await transporter.sendMail(mailOptions);
+    await transporter.sendMail(mailOptions1);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "E-mail envoyé avec succès!" }),
+    };
+  } catch (error) {
+    console.error("Error", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Une erreur est survenue lors de l'envoi de l'e-mail.",
+      }),
+    };
+  }
 };
-
-// Utilisez netlify-lambda pour exporter la fonction serverless
-module.exports.handler = serverless(exports.handler);
